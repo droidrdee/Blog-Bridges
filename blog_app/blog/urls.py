14bff8dd31a12166fea48a7c549afc4aa44cf192
deepakp-rathore <deepak.yourdomain@gmail.com>
2023-08-13 00:42:17 +0530

    Added views of home, login, logout, newpost

--- blog_app/blog/urls.py
+++ blog_app/blog/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login-page'),
-    path('signup/', views.signup, name='signup-page'),
+    path('', views.signup, name='signup-page'),
+    path('loginn/', views.loginn, name='login-page'),
     path('home/', views.home, name='home-page'),
+    path('newpost/', views.newPost, name='new-post'),
+    path('mypost/', views.myPost, name='my-post'),
+    path('signout/', views.signout, name='signout-btn'),
+    
 ]
--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -1,15 +1,73 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from blog import models
+from .models import Post
+from django.contrib.auth import authenticate, login, logout
 
-def home(request):
-    return render(request, 'blog/home.html')
 
-
-def login(request):
-    return render(request, 'blog/login.html')
 
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        email= request.POST.get('uemail')
+        password = request.POST.get('upassword')
+        newUser = User.objects.create_user(username=name, email=email, password=password)
+        newUser.save()
+        return redirect('/loginn')
     return render(request, 'blog/signup.html')
 
 
+
+
+def loginn(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        password = request.POST.get('upassword')
+        userr = authenticate(request, username=name, password=password)
+        if userr is not None:
+            login(request, userr)
+            return redirect('/home')
+        else:
+            return redirect('/login')
+
+    return render(request, 'blog/login.html')
+
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+
+def newPost(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        npost = models.post(title=title, content=content, auther=request.user)
+        npost.save()
+        return redirect('/home')
+    
+    return render(request, 'blog/newpost.html')
+
+
+
+
+def myPost(request):
+    context = {
+        'posts': Post.objects.filter(author= request.user)
+    }
+    return render(request, 'blog/mypost.html', context)
+
+
+
+def signout(request):
+    if request.method == 'POST':
+        logout(request, user=request.auther)
+        return redirect('/loginn')
+    
+    return render(request, 'blog/home.html')
